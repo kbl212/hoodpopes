7fc53f8dc8ef51d76a55e6fb4249d6377d6ade4f
Kyle <kyle.lauritzen@gmail.com>
2015-11-20 22:55:31 -0700

    figured out routes, as well as a temp logo, and buttons for layouts/strats

--- public/js/controllers/layoutsCtrl.js
+++ public/js/controllers/layoutsCtrl.js
@@ -3,6 +3,25 @@ var app = angular.module('hoodPopes');
 
 app.controller('layoutsCtrl', function($scope) {
     
+    var th5Layouts = ["testing", "testing2", "testing3"];
+        var th6Layouts = ["testing", "testing2", "testing3"];
+        var th7Layouts = ["testing", "testing2", "testing3"];
+        var th8Layouts = ["testing", "testing2", "testing3"];
+        var th9Layouts = ["testing", "testing2", "testing3"];
     
+    $scope.th5 = th5Layouts;
+    $scope.fiveShowing = false;
+    
+        $scope.th6 = th6Layouts;
+    $scope.sixShowing = false;
+    
+        $scope.th7 = th7Layouts;
+    $scope.sevenShowing = false;
+    
+        $scope.th8 = th8Layouts;
+    $scope.eightShowing = false;
+    
+        $scope.th9 = th9Layouts;
+    $scope.nineShowing = false;
     
 });
